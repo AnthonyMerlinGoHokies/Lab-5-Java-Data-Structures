05a94ade443a6018392f5ea01b9197d584c7097f
Anthony Merlin <92953379+AnthonyMerlinGoHokies@users.noreply.github.com>
2022-10-22 09:29:54 -0400

    Add files via upload

---
+++ CommandPromptExercise.java
@@ -0,0 +1,26 @@
+package icecream;
+
+/**
+ * Gain experience running Java programs from the command prompt by getting the
+ * Strings below to print in the Command Prompt terminal.
+ * 
+ * From the src directory: To compile: javac
+ * icecream\CommandPromptExercise.java To run: java
+ * icecream.CommandPromptExercise
+ *
+ * @author Anthony Merlin
+ * @version 2022.09.21
+ */
+public class CommandPromptExercise {
+    public static void main(String args[]) {
+        String flavors = "My favorite ice cream flavors are:";
+        String choco = "Chocolate";
+        String van = "Vanilla";
+        String mint = "Mint Chocolate Chip";
+
+        System.out.println(flavors);
+        System.out.println(choco);
+        System.out.println(van);
+        System.out.println(mint);
+    }
+}
